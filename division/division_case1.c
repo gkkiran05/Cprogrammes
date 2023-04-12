d321b23b25434de40aa5b8e31c7033851feeff0e
Kiran <kirankumargk05@gmail.com>
2023-04-12 20:34:17 +0000

    Division

---
+++ division/division_case1.c
@@ -0,0 +1,22 @@
+//divison programme
+#include <stdio.h>
+
+int main()
+{
+	//variable declaration
+	int i4value1;
+	int i4value2;
+	int i4multi;
+
+	//variable intilization
+	i4value1 = 10;
+	i4value2 = 2;
+
+	//main logic
+	i4multi = i4value1 / i4value2;
+
+	//printing out put on screen
+	printf("%d\n", i4multi);
+
+	return 0;
+}
