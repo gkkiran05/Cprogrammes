660cb17362f9bac35e970ec526f7b7d55c39b62c
Kiran <kirankumargk05@gmail.com>
2023-04-18 08:01:27 +0000

    Format specifiers

---
+++ format_specifiers/Format_specifier1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	//Variable declaration and intilization
+	 int i = 1234567;
+	 int a = 0;
+	 int b = -1;
+
+	 //printing out put
+	 
+	 printf("%d %d %d\n", i, a, b);
+	 printf("%c %c %c\n", i, a, b);
+	 printf("%x %x %x\n", i, a, b);
+	 printf("%o %o %o\n", i, a, b);
+
+	 return 0;
+
+}
+
+
+/*
+ * out put
+ * --------
+ *  1234567  0  -1
+ *
+ *  12d687  0 fffffffff
+ *  4553207 0 3777777777
+ **/
+
+
---
+++ format_specifiers/Format_specifier2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+	//variable declaration and intilization
+	char a = 'A';
+	char b = 'a';
+	char c = 7;
+
+	//printing out put
+	printf("%d %d %d\n", a, b, c);
+	printf("%c %c %c\n", a, b, c);
+	printf("%x %x %x\n", a, b, c);
+	printf("%o %o %o\n", a, b, c);
+	
+	return 0;
+}
+
+
+
+/*
+ * out put
+ * -------
+ *65 97 7
+ A a
+ 41 61 7
+ 101 141 7
+ */
+
+
