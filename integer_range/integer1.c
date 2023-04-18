8a43e5e91030e6c7e4d6583083d169c64ce969b0
Kiran <kirankumargk05@gmail.com>
2023-04-18 08:13:40 +0000

    intergers_range

---
+++ integer_range/integer1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	//varaible declaration
+	int a = 2147483647;
+
+	//adding 1 to varaible
+	a = a + 1;
+
+	//printing out put
+	printf("%d\n", a);
+
+	return 0;
+}
+
+
+/* 
+ * out put
+ * -------
+ *  -214483647
+ *  */
+
---
+++ integer_range/integer2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+        //varaible declaration
+        unsigned int a = 4294967295;
+
+        //adding 1 to varaible
+        a = a + 1;
+
+        //printing out put
+        printf("%d\n", a);
+
+        return 0;
+}
+
+
+
+/*
+ * ou put
+ * ------
+ *  0
+ *  */
