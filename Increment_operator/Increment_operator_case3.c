1aa4744bbd9e66deb57cffb7d65ee4c2c5de1f5b
Kiran <kirankumargk05@gmail.com>
2023-04-21 17:22:54 +0000

    Post And Pre Increment

---
+++ Increment_operator/Increment_operator_case3.c
@@ -0,0 +1,38 @@
+//increment operator
+// a++ post increment
+// ++a pre increment
+
+#include <stdio.h>
+
+int main()
+{
+    //Declaring And Intializing Variables
+
+    int a = 10;
+    int b;
+
+    //Main Logic
+
+    b = ++a + ++a;
+
+    //printing a and b value after the expression
+
+    printf("The Value of a is : %d\n", a);
+    printf("The Value of b is : %d\n", b);
+
+    return 0;
+
+}
+
+
+
+/*
+ *     out put
+ *-----------------
+  The Value of a is : 12
+  The Value of b is : 24
+*/
+
+
+
+
