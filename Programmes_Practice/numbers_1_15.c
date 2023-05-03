322a932480c7d991c510e8b37f7ebf83ccd775da
Kiran <kirankumargk05@gmail.com>
2023-05-03 10:31:05 +0000

    Printing the numbers from 1 to 15

---
+++ Programmes_Practice/numbers_1_15.c
@@ -0,0 +1,40 @@
+//Programme For Printing 1 to 15 numbers
+
+#include  <stdio.h>
+
+int main()
+{
+    //Variable Declaration and intialization
+    int i = 1;
+    int num = 15;
+
+    //Main Logic
+    for(;i <= num; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+
+
+
+/*
+ *   out put
+ *------------
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+ 10
+ 11
+ 12
+ 13
+ 14
+ 15
+*/
