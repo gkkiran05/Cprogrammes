dfc26b21f460b79fa29894407a577ce6cedf5392
Kiran <kirankumargk05@gmail.com>
2023-05-10 02:57:41 +0000

    hollow reverse pyramid

---
+++ Loops/Patterns/hollow_reverse_pyramid.c
@@ -0,0 +1,59 @@
+//Programme To Print Reverse Pyramid
+
+#include <stdio.h>
+
+int main()
+{
+    //Varaible Declaration
+    int num;
+
+    //Taking input From user and store in num varaible
+    printf("Enter The Value : ");
+    scanf("%d", &num);
+    
+    //For Printing Rows
+    for(int i = 1; i<= num; i++)
+    {
+        //For Printing Spaces
+        for(int j = 1; j < i; j++)
+        {
+            printf("  ");
+        }
+
+        //For Printing stars
+        for(int k = 1; k <= (num * 2 -(2 * i - 1)); k++)
+        {
+	    //For printing stars
+            if((i == 1) || (k == (num * 2) - (2 * i - 1)) || (k == 1))
+            {
+                printf(" *");
+            }
+	    //For Printing Hollow
+            else
+            {
+                printf("  ");
+            }
+            
+        }
+
+        //For Printing new line after coloum
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
+/*
+ *   out put 
+ *--------------
+ Enter The Value : 5
+ * * * * * * * * *
+   *           *
+     *       *
+       *   *
+         *
+*/
+
