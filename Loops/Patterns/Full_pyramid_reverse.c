f610bd63ab2b529620edc11a79ec9f89a05e5d70
Kiran <kirankumargk05@gmail.com>
2023-05-10 02:40:39 +0000

    Full pyramid reverse

--- Loops/Patterns/Full_pyramid_reverse.c
+++ Loops/Patterns/Full_pyramid_reverse.c
@@ -1,46 +1,49 @@
+//Programme To Print Reverse Pyramid
+
 #include <stdio.h>
 
 int main()
 {
-    //Variable Declaration
+    //Varaible Declaration
     int num;
 
-    //Taking input from user and store in num variable
-    printf("Enter The Value :");
+    //Taking input From user and store in num varaible
+    printf("Enter The Value : ");
     scanf("%d", &num);
-
-    /*Main Logic
-     * Outer Loop For Rows*/
-    for(int i = 1; i <= num; i++)
+    
+    //For Printing Rows
+    for(int i = 1; i<= num; i++)
     {
-        //First Inner Loop For Printing Spaces
-        for(int j = 0; j < i; j++)
+        //For Printing Spaces
+        for(int j = 1; j < i; j++)
         {
             printf("  ");
         }
-        //Second Inner Loop for Printing stars
-        for(int k = num; k >=  (i * 2) - 1; k--)
+
+        //For Printing stars
+        for(int k = 1; k <= (num * 2 -(2 * i - 1)); k++)
         {
             printf(" *");
         }
+
+        //For Printing new line after coloum
         printf("\n");
     }
+
     return 0;
 }
 
 
 
 
-
 /*
- *   out put
+ *   out put 
  *---------------
-   Enter The Value :10
-   * * * * * * * * *
-     * * * * * * *
-       * * * * *
-         * * *
-           *
-
+ Enter The Value : 5
+ * * * * * * * * *
+   * * * * * * *
+     * * * * *
+       * * *
+         *
 */
 
