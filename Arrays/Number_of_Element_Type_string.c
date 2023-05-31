7a33dcfe58852b8aa55e2691e6f44f04de44a18a
Kiran <kirankumargk05@gmail.com>
2023-05-31 02:16:06 +0000

    Elements in strings type

--- Arrays/Number_of_Element_Type_string.c
+++ Arrays/Number_of_Element_Type_string.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 int main()
 {
+    //Declaration
     char c[10];
     int digit = 0;
     int alphabet = 0;
     int special = 0;
     int size_of = sizeof(c)/sizeof(c[0]);
     
+    //Taking the values into Array
     printf("Enter The characters:");
     scanf("%s", c);
     
     //Loop For Checking The Elemets in array
     for (int j = 0; j < size_of; j++)
     {
+	//Checking Digits in Array
         if ((c[j] >= '0') && (c[j] <= '9'))
         {
             digit = digit + 1;
         }
+	//Checking Alphabets in Array
         else if ((c[j] >= 'A') && (c[j] <= 'Z') || (c[j] >= 'a' && c[j] <= 'z'))
         {
             alphabet = alphabet + 1;
         }
+	//Checking Special Character in Array
         else
         {
             special = special + 1;
